dec01a2b4d0c809c361cd5ad4715f5e8fd791198
Andre_Victor <95365578+AndreVsc@users.noreply.github.com>
2023-10-18 09:18:05 +0000

    Add express and local

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+//  Inicializando express
+const express = require('express');
+const app = express();
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+
+// Colocando aplicação online
+app.listen(3000);
