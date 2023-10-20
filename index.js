45c577a23f754689d75ed9eb02e09b6cb30ee1f1
AndreVsc <vsandre40@gmail.com>
2023-10-20 01:26:08 -0300

    Implement endpoint 'ReadAll'+"Create"

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+// Sinaliza para o para o Express que sempre estara em JSON
+app.use(express.json());
+
 app.get('/',function(req,res){
     res.send('Hello World');
 });
@@ -15,11 +18,31 @@ app.get('/test', function(req,res){
 
 const items = ["Java","Android","Kotlin","Express","NestJs"];
 
+// -----------------------------------------------------------------------
+
 // READ ALL 
 
 app.get("/items", function(req,res){
     res.send(items);
 });
+
+// CREATE 
+
+app.post("/items", function(req,res){
+    
+    //  Extrair a informação do corpo da requisição
+    const item = req.body.nome;
+
+    // Inserir item na lista
+    items.push(item);
+    res.send("Item created sucessfully");
+})
+
+// 
+
+
+
+//------------------------------------------------------------------------
 
 // Colocando aplicação online
 app.listen(3000 , function(){
