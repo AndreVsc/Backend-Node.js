1343459e51ff8e13ec1077816236d99028b40f7e
AndreVsc <vsandre40@gmail.com>
2023-10-20 01:00:25 -0300

    endpoint Read all

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ app.get('/test', function(req,res){
 });
 
 
+// Crud de lista e criaturas
+
+const items = ["Java","Android","Kotlin","Express","NestJs"];
+
+// READ ALL 
+
+app.get("/items", function(req,res){
+    res.send(items);
+});
+
 // Colocando aplicação online
 app.listen(3000 , function(){
     console.log("App running on http://localhost:3000");
